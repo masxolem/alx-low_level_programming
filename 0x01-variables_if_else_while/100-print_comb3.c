d7da405dc47f0b92dd50f94640418b3b9a918e7a
Masixole Mgqibanda <masxolem9@gmail.com>
2023-06-18 20:06:10 -0700

    kj

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,23 +6,24 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int main()
 {
-	int digit1, digit2;
-
-	for (digit1 = 0; digit1 < 9; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 8 && digit2 == 9)
-				continue;		
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+    int i, j;
+    
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = i + 1; j <= 9; j++)
+       	{
+            putchar('0' + i);
+            putchar('0' + j);
+            
+            if (i != 8 || j != 9)
+	    {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    
+    return 0;
 }
